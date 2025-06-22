5dd665c87f5b622f9ab5d37d3e32e3be1e96df91
Goncalo <tiagohoshi1@gmail.com>
2025-06-22 17:59:44 +0100

    Fix header-wrapper and Stripe webhook route

--- components/header-wrapper.tsx
+++ components/header-wrapper.tsx
@@ -1,44 +1,10 @@
-PS C:\Users\Goncalo\pombot> // components/header-wrapper.tsx
-// : The term '//' is not recognized as the name of a cmdlet, function, script file, or operable 
-program. Check the spelling of the name, or if a path was included, verify that the path is 
-correct and try again.
-At line:1 char:1
-+ // components/header-wrapper.tsx
-+ ~~
-    + CategoryInfo          : ObjectNotFound: (//:String) [], CommandNotFoundException      
- 
-PS C:\Users\Goncalo\pombot> import { cookies } from 'next/headers'
-import : The term 'import' is not recognized as the name of a cmdlet, function, script file, or    
-operable program. Check the spelling of the name, or if a path was included, verify that the path  
-is correct and try again.
-At line:1 char:1
-+ import { cookies } from 'next/headers'
-+ ~~~~~~
-    + CategoryInfo          : ObjectNotFound: (import:String) [], CommandNotFoundException
- 
-PS C:\Users\Goncalo\pombot> import { auth } from '@/auth'
-import : The term 'import' is not recognized as the name of a cmdlet, function, script file, or    
-operable program. Check the spelling of the name, or if a path was included, verify that the path  
-is correct and try again.
-At line:1 char:1
-+ import { auth } from '@/auth'
-+ ~~~~~~
-    + CategoryInfo          : ObjectNotFound: (import:String) [], CommandNotFoundException
- 
-PS C:\Users\Goncalo\pombot> import { HeaderClient } from './header-client'
-import : The term 'import' is not recognized as the name of a cmdlet, function, script file, or    
-operable program. Check the spelling of the name, or if a path was included, verify that the path  
-is correct and try again.
-At line:1 char:1
-+ import { HeaderClient } from './header-client'
-+ ~~~~~~
-    + CategoryInfo          : ObjectNotFound: (import:String) [], CommandNotFoundException
-    + FullyQualifiedErrorId : CommandNotFoundException
- 
-PS C:\Users\Goncalo\pombot>
-PS C:\Users\Goncalo\pombot> export async function HeaderWrapper() {
->>   const cookieStore = cookies()
->>   const session = await auth({ cookieStore })
->> 
->>   return <HeaderClient session={session} />
->> }
+import { cookies } from 'next/headers'
+import { auth } from '@/auth'
+import { HeaderClient } from './header-client'
+
+export async function HeaderWrapper() {
+  const cookieStore = cookies()
+  const session = await auth({ cookieStore })
+
+  return <HeaderClient session={session} />
+}
