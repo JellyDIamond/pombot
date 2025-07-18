28496d683bcaaddd5e3b8a5b51becda71b251a97
JellyDIamond <79990446+JellyDIamond@users.noreply.github.com>
2025-07-18 10:55:19 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -11,5 +11,15 @@ module.exports = {
         hostname: '**.githubusercontent.com'
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'utf-8-validate': false,
+        'bufferutil': false
+      }
+    }
+    return config
   }
 }
