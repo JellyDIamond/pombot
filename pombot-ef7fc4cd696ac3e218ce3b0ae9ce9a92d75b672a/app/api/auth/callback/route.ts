09411a144c366e9ea09db336007876329f9939db
JellyDIamond <79990446+JellyDIamond@users.noreply.github.com>
2025-07-21 13:30:50 +0100

    n

--- pombot-ef7fc4cd696ac3e218ce3b0ae9ce9a92d75b672a/app/api/auth/callback/route.ts
+++ pombot-ef7fc4cd696ac3e218ce3b0ae9ce9a92d75b672a/app/api/auth/callback/route.ts
@@ -2,7 +2,6 @@ import 'server-only'
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
@@ -21,3 +20,4 @@ export async function GET(request: Request) {
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin)
 }
+
